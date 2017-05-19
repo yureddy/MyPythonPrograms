df48cc39e61bc956c32ec152e674a4fe2f07ba4f
Yugandhar <ygndr.reddy@gmail.com>
2017-05-19 18:33:04 -0400

    just an updated version

--- Filehandling.py
+++ Filehandling.py
@@ -1,22 +1,74 @@
-fp = open("MyFirstFile.txt","r+")
-#print(fp)
+input = open('BigtextFile.txt','r')
+Output_File = open('output.txt','w')
 
-#fp.write("**** I am writing from the program Filehandling.py**** \n")
-#fp.write("****This is second line i am writeing from a program to MyFirstFile.txt*****")
-
-
-#print("I am printing fp.read")
-#print(fp.read())
-
-print("I am printing fp.readline")
-print(fp.readline())
-
-print("I am printing fp.readlines")
-print(fp.readlines())
+#while(1):
+for lines in range(100000):
+		line = input.readline()
+		Output_File.write(line)
+	#user_input = input("Type Stop to Quit, else press any key to continue")
+	#if user_input == 'STOP':
+	#	break
 
 
-Lines_oF_Text = ['**New Line 1 Added** \n', '**New Line 2 Added** \n','**New Line 3 Added** \n']
-fp.writelines(Lines_oF_Text)
-fp.close()
+
+#To run OS commands from Python
+#import os
+#ToIdentify = os.system('ls -la')
+#print(ToIdentify)
+
+#Copying from one file and creating and writing the whole content to it
+#pass the command line as  ##$"python Filehandling.py MyFirstFile.txt Duplicate.txt"
+#import sys
+#f len(sys.argv) < 3:
+#	print("wrong Prameter")
+#	print("Give the console command in the form python Filehandling.py file1 file2")
+#	sys.exit()
+
+#print("you have entered sys.argv[0] is "+sys.argv[0] + "sys.argv[1] is" +sys.argv[1]+ "sys.argv[2] is"+sys.argv[2] )
+#f1 = open(sys.argv[1])
+#File1Content=f1.readlines()
+#print(File1Content)
+#f1.close()
+#f2 = open(sys.argv[2],'w')
+#f2.writelines(File1Content)
+#f2.close()
 
 
+
+#program which takes 
+#files names as inpout from the user and show the 
+#content of the files in console
+
+#FilesName = input("Enter the file name ")
+#FilesObject = open(FilesName)
+#print(FilesObject.read())
+#FilesObject.close()
+
+
+
+#with open("MyFirstFile.txt") as myNewFilePointer:
+#	for line in myNewFilePointer:
+#		print (line)
+# we dont need to explicitly close a filepointer, if we use with to open file
+
+
+
+
+#if you do not provide any mode, the file will be open in read only mode
+#You should always explicitly close the opened file, as there is a limit
+#on how many files can be opned at a time on sys, so system can crash if 
+#if the limit cross, 
+#Each open file consumes some amount of main memoery for data structures
+#associated with it like file descriptor/handle or file locks etc,so u
+#may end up opening lot of files wasting lot of memory if you have more 
+#files that are not useful or usable. Open files stand a chance of 
+#curroption and data loss
+
+
+#*** to show usage of for loop
+#fp = open("MyFirstFile.txt","a")
+#for line in fp:
+  #print(line)
+
+#fp.write("Line 7")
+#fp.close()
