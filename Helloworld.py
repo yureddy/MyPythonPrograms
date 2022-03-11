7b55dbf334bd03bb940a42edeac185e4030ae44c
Yugandharreddy Sanikommu <yreddy@vectormax.com>
2022-03-11 16:54:28 -0500

    pushing python learnings

--- Helloworld.py
+++ Helloworld.py
@@ -1,2 +1,89 @@
 print("Hello world")
 
+    """
+    Important concepts from python.org i
+    Using and Invoking python interpreter
+    Argument passing
+    
+    
+    Control Flow
+        Cover functions also here.
+        
+    Data Strucures
+        Lists as Stacks and Queues
+        
+    Modules
+        Executing modules as scripts
+        Module search path]
+        "Compiled" python files to
+        
+        Standard modules 
+        dir() functions
+        Packages
+            Importing * from a package to
+            Intra-package References
+            Packages in multiple directories.
+            
+    Input and Output
+        Output formatting
+        Formatting string literals
+        string format() methods.
+        Manual string formatting
+        Old string formatting
+        
+    Reading and writing files to
+        Methods of File objects
+        Saving structured data with json
+        
+    Errors and exceptions
+        Syntax errors and
+        Exceptions
+        Handling exceptions.
+        Raising exceptions.
+        Exception chaining
+        user-defined exceptions.
+        Defining clean-up actions 
+        predefined cleanup actions.
+        
+    Classes
+        Names and objects
+        Python Scopes and Namespace
+        Class objects
+        Instance objects
+        Methods objects
+        Class and Instance variables
+        Inheritance
+            Multiple inheritence
+        Private Variables
+        Iterators
+        Generators
+        Generator expressions
+        
+    Standard libraries
+        Operating system interfaces as
+        File wildcards
+        Command line arguments.
+        Error output redirection and program termination
+        string pattern mathcing
+        Internet access 
+        Dates and Times
+        Data Compression
+        Performance Measurement
+        Quality control
+        
+    Brief touf of STD Library
+        Output formatting
+        Templating
+        Working with binary data record layouts
+        multi-threading
+        logging
+        Weak references
+        Tools for working with lists
+        Decimal floating point arithematic
+    
+    Virtual Environmentand Packages
+        Creating virtual environment
+        Managing packags with pipe
+        
+    
+    """
---
+++ OOP.py
@@ -0,0 +1,7 @@
+#Class Inheritance mechanism provides multiple base classees,
+# a derived class can override any methods of its base class or classes, 
+#and a method can call the method of a base class with same name
+
+#Objects can contain arbitratry and kinds of data.add
+#Dynamic nature of pyython: they can be created ar runtimt en further changes as needed
+
